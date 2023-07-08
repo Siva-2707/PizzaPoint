0af003c4b20459fcac12f8e248e54c7f7f3e8522
Siva-2707 <b.sivarajesh20@gmail.com>
2023-07-09 00:35:46 +0530

    Initial file

---
+++ pizza-point/src/main/java/com/siva/pizzapoint/PizzaPointApplication.java
@@ -0,0 +1,13 @@
+package com.siva.pizzapoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPointApplication.class, args);
+	}
+
+}
---
+++ pizza-point/src/test/java/com/siva/pizzapoint/PizzaPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siva.pizzapoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
